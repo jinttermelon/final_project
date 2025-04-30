668824810ceb18aa3215da1d40688b187e32a8c3
jintermelon <sudalsubak@gmail.com>
2025-04-30 15:25:08 +0900

    chat 기능 연결(1)

---
+++ src/main/java/org/multi/final_project/chat/ChatController.java
@@ -0,0 +1,56 @@
+package org.multi.final_project.chat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    @Autowired
+    private ChatMapper chatMapper;
+
+    public ChatController(ChatMapper chatMapper) {
+        log.info("ChatController");
+        this.chatMapper = chatMapper;
+    }
+
+    @GetMapping("/rooms")
+    public String findAllRooms(Model model) {
+        List<ChatRoomVO> rooms = chatMapper.findAllRooms();
+        log.info("rooms.size() : {}",rooms.size());
+        model.addAttribute("rooms", rooms);
+        return "chat/rooms";
+    }
+
+    @PostMapping("/room")
+    public String createRoom(ChatRoomVO room) {
+        log.info("Creating roomName : {}", room);
+        room.setRoom_id(UUID.randomUUID().toString());
+        log.info("Creating roomName : {}", room);
+
+        chatMapper.insertRoom(room);
+        return "redirect:/chat/rooms";
+    }
+
+    @GetMapping("/room/{room_id}")
+    public String enterRoom(@PathVariable String room_id, Model model) {
+        log.info("enterRoom : {}", room_id);
+        model.addAttribute("room_id", room_id);
+        model.addAttribute("messages_history", chatMapper.getMessagesByRoomId(room_id));
+        return "chat/room";
+    }
+}
+
+
+
---
+++ src/main/java/org/multi/final_project/chat/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package org.multi.final_project.chat;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Component
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    public WebSocketConfig(ChatWebSocketHandler chatWebSocketHandler) {
+        this.chatWebSocketHandler = chatWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+}
+
+
