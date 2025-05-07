6cfa0c08c1ad4b405b7cd65584c891a423d4c68c
jintermelon <sudalsubak@gmail.com>
2025-05-07 14:13:21 +0900

    5/7 채팅 기능 최신버전

--- src/main/java/org/multi/final_project/chat/ChatController.java
+++ src/main/java/org/multi/final_project/chat/ChatController.java
@@ -1,6 +1,8 @@
 package org.multi.final_project.chat;
 
 import lombok.extern.slf4j.Slf4j;
+import org.multi.final_project.user.UserMapper;
+import org.multi.final_project.user.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.UUID;
+
 
 @Slf4j
 @Controller
@@ -19,6 +23,9 @@ public class ChatController {
 
     @Autowired
     private ChatMapper chatMapper;
+
+    @Autowired
+    private UserMapper userMapper;
 
     public ChatController(ChatMapper chatMapper) {
         log.info("ChatController");
@@ -44,10 +51,16 @@ public class ChatController {
     }
 
     @GetMapping("/room/{room_id}")
-    public String enterRoom(@PathVariable String room_id, Model model) {
-        log.info("enterRoom : {}", room_id);
+    public String enterRoom(@PathVariable String room_id, Model model, Principal principal) {
+//        if (principal == null) {
+//            return "redirect:/user/login"; // 로그인 안 했으면 로그인 페이지로
+//        }
+        String dummyNickname = "더미유저";
+        model.addAttribute("nickname", dummyNickname);
+
         model.addAttribute("room_id", room_id);
         model.addAttribute("messages_history", chatMapper.getMessagesByRoomId(room_id));
+
         return "chat/room";
     }
 }
