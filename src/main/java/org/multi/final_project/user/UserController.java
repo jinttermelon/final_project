9104579d345fb18215dd6f84c23cbae779df7fb4
innngjuu <allaround24@naver.com>
2025-05-12 13:08:47 +0900

    마이페이지

--- src/main/java/org/multi/final_project/user/UserController.java
+++ src/main/java/org/multi/final_project/user/UserController.java
@@ -34,6 +34,11 @@ public class UserController {
         return "user/insert";
     }
 
+    @GetMapping("/mypage")
+    public String mypage() {
+        return "user/mypage";
+    }
+
     @GetMapping("/findId")
     public String findId() {
         return "user/findId";
