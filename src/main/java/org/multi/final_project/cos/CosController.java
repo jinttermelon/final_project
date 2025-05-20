d21be7be188e6b2d1185963deabb77e18b493255
yangssem <yangssem88@gmail.com>
2025-05-20 10:29:59 +0900

    코스등록 처리수정,코스테이블수정,홈에서코스이미지처리수정

--- src/main/java/org/multi/final_project/cos/CosController.java
+++ src/main/java/org/multi/final_project/cos/CosController.java
@@ -9,6 +9,7 @@ import org.multi.final_project.cosreview.CosReviewVO;
 import org.multi.final_project.crewboard.CrewBoardVO;
 import org.multi.final_project.event.EventVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Slf4j
@@ -31,6 +37,9 @@ public class CosController {
 
     @Autowired
     private HttpSession session;
+
+    @Value("${file.dir}")
+    private String realPath;
 
     @GetMapping("/cosManagement")
     public String cosManagement(@RequestParam(defaultValue = "1") int cpage,
@@ -67,8 +76,37 @@ public class CosController {
     }
 
     @PostMapping("/insertOK")
-    public String insertOK(CosVO vo){
+    public String insertOK(CosVO vo) throws IOException {
+        log.info("vo:{}",vo);
         log.info("insertOK()...");
+
+        log.info("realPath : {}",realPath);
+
+        String originName = vo.getFile().getOriginalFilename();
+        log.info("originName : {}",originName);
+
+        if(originName.length() == 0){//파일첨부안되었을때는 기본이미지이름으로 설정.
+            vo.setImg_name("default.png");
+        }else{
+            //중복파일명 배제하는 처리. ex: img_387483924732743.png
+            String save_name = "img_"+ System.currentTimeMillis()+originName.substring(originName.lastIndexOf("."));
+            log.info("save_name : {}",save_name);
+            vo.setImg_name(save_name);//디비에 들어갈 이미지명 세팅
+
+            File f = new File(realPath,save_name);
+            vo.getFile().transferTo(f);//파일 저장...
+
+            //작은이미지로 만들어서 저장하기
+            //// create thumbnail image/////////
+            BufferedImage original_buffer_img = ImageIO.read(f);
+            BufferedImage thumb_buffer_img = new BufferedImage(50, 50, BufferedImage.TYPE_3BYTE_BGR);
+            Graphics2D graphic = thumb_buffer_img.createGraphics();
+            graphic.drawImage(original_buffer_img, 0, 0, 50, 50, null);
+
+            File thumb_file = new File(realPath, "thumb_" + save_name);
+
+            ImageIO.write(thumb_buffer_img, save_name.substring(save_name.lastIndexOf(".") + 1), thumb_file);
+        }//end if
 
         service.insertOK(vo);
         log.info(vo.toString());
