481821ed2e13e06aa673c6ffad30a3633d59c729
lee tae young <lee_tae0@naver.com>
2025-05-20 15:00:58 +0900

    커뮤니티

--- src/main/java/org/multi/final_project/chat/ChatController.java
+++ src/main/java/org/multi/final_project/chat/ChatController.java
@@ -1,5 +1,6 @@
 package org.multi.final_project.chat;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.multi.final_project.friend.FriendMapper;
 import org.multi.final_project.user.UserMapper;
@@ -18,36 +19,30 @@ public class ChatController {
 
     @Autowired
     private ChatMapper chatMapper;
-    private final UserMapper userMapper;
-    private final FriendMapper friendMapper;
 
-    public ChatController(ChatMapper chatMapper, UserMapper userMapper, FriendMapper friendMapper) {
-        this.chatMapper = chatMapper;
-        this.userMapper = userMapper;
-        this.friendMapper = friendMapper;
-    }
+    @Autowired
+    private FriendMapper friendMapper;
 
-    //채팅방 전체 목록 출력
-    @GetMapping("/rooms")
-    public String findAllRooms(Model model) {
-        List<ChatRoomVO> rooms = chatMapper.findAllRooms();
-        model.addAttribute("rooms", rooms);
-        return "chat/rooms"; // 방 목록만 있는 별도 페이지가 있다면
-    }
+    @Autowired
+    private HttpSession session;
+
+
 
     //채팅방 입장 (room_id 파라미터 받기)
-    @GetMapping("/room")
-    public String enterRoom(@RequestParam(name = "room_id", required = false) String roomId, Model model) {
-        if (roomId != null) {
-            ChatRoomVO selectedRoom = chatMapper.selectRoomById(roomId);
-            model.addAttribute("selectedRoom", selectedRoom);
+    @GetMapping("/cummunity")
+    public String cummunity(@RequestParam(name = "room_id", required = false) String room_id, Model model) {
+//        if (roomId != null) {
+//            ChatRoomVO selectedRoom = chatMapper.selectRoomById(roomId);
+//            model.addAttribute("selectedRoom", selectedRoom);
+//
+//            List<ChatMessageVO> messages = chatMapper.selectMessagesByRoomId(roomId);
+//            model.addAttribute("messages", messages);
+//        }
 
-            List<ChatMessageVO> messages = chatMapper.selectMessagesByRoomId(roomId);
-            model.addAttribute("messages", messages);
-        }
-
-        model.addAttribute("chatRooms", chatMapper.findAllRooms());
-        model.addAttribute("friends", friendMapper.selectAcceptedFriends("name03")); // 테스트용 유저
+        List<ChatRoomVO> rooms = chatMapper.findAllRooms();
+        model.addAttribute("rooms", rooms);
+        model.addAttribute("messages_history", chatMapper.getMessagesByRoomId(room_id));
+        model.addAttribute("friends", friendMapper.selectAcceptedFriends(session.getAttribute("nickname").toString()));
         return "chat/chatMain";
     }
 
@@ -56,8 +51,6 @@ public class ChatController {
     public String insertRoom(ChatRoomVO room) {
         room.setRoom_id(UUID.randomUUID().toString());
         chatMapper.insertRoom(room);
-        return "redirect:/chat/room?room_id=" + room.getRoom_id();
+        return "redirect:/chat/cummunity?room_id=" + room.getRoom_id();
     }
-
-
 }
